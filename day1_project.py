c3daada52ae535a2c0db11f8efa92791733bddcd
Tobias <tobiascosta064@gmail.com>
2025-02-05 15:03:57 -0300

    feat: first daily python project concluded

---
+++ day1_project.py
@@ -0,0 +1,5 @@
+#Gerador de nome de banda
+print("Olá, bem vindo ao gerador de nome de banda!\nAqui você conseguirá descobrir o nome perfeito para a sua banda.")
+city = input("Em qual cidade você cresceu?\n")
+pet_name = input("Qual o nome do seu pet?\n")
+print("O nome da sua banda poderia ser: " + city + " " + pet_name)
