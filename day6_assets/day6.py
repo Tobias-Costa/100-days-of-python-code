b47ff0770d250a785ade467eaf3131afe2f85a3d
Tobias <tobiascosta064@gmail.com>
2025-02-12 16:34:29 -0300

    feat: day 6 project done(Reeborg's World)

---
+++ day6_assets/day6.py
@@ -0,0 +1,23 @@
+#Functions available:
+# move() turn_left().
+# front_is_clear() or wall_in_front()
+# right_is_clear() or wall_on_right() and at_goal().
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left() 
+    
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+    
